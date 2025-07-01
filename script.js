bfb72c71122125323fecc82dba3461c24662a671
carolgeremias <146000560+carolgeremias@users.noreply.github.com>
2025-07-01 11:37:01 -0300

    aula 01|07

---
+++ script.js
@@ -0,0 +1,30 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:Arthur achou o mapa do tesouro, e partiu para caçar O Tesouro, e viu que no mapa tem riscos marrons e verdes
+        alternativas: [
+            "ir para a floresta"
+            "ir para a prisão"
+        ]
+    },
+    {
+        enunciado:Arthur caminhou pela floresta, viu que no mapa, tinha uma caverna e seguindo ele achou a caverna.
+        alternativas: [
+            "ir para a caverna"
+            "ir para embora"
+        ]
+    },
+    {
+        enunciado:
+        alternativas: [
+            "ir para a floresta"
+            "ir para a prisão"
+        ]
+    },
+    }
+]
