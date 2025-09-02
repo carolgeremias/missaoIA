94d993e2625af726b4c2e75cde423f27d20e3de2
carolgeremias <146000560+carolgeremias@users.noreply.github.com>
2025-09-02 11:29:02 -0300

    hrmrfermjfyjmrfy

--- script.js
+++ script.js
@@ -23,51 +23,49 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Utiliza uma ferramenta"
-                afirmacao: "vc corta o cercado e vai para a floresta.
+                afirmacao: "vc corta o cercado e vai para a floresta"
                 "
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "ignorar e voltar",
+                afirmacao: "você volta para a casa"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
+        enunciado: "após você seguir a floresta você encontra 2 caminhos
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "caminho 1",
+                afirmacao: "você acha a caverna"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "caminho 2",
+                afirmacao: "você não acha a caverna"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "o mapa do tesouro te leva para a caverna, você segue e acha um 'X' ",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "cavar",
+                afirmacao: "você acha um baú"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
+                texto: "não cavar",
+                afirmacao: "você não cava"
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "você pega o baú do tesouro após abrir, acha vários tesouros, mas vocÊ se pergunta sobre oque deve fazer.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Doar todo o tesouro para a caridade",
+                afirmacao: "Após fazer isso, você se sente bem com isso, e as crianças carentes ficam felizes!
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Gastar tudo no Tigrinho",
+                afirmacao: "Ele ameaçou jogar a carta mas não jogou e perdeu tudo, FIM! (OTÁRIO) "
             }
         ]
     },
